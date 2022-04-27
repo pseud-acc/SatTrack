fa991b04fd228bd28162730707871b6e36093f25
Pseud-F <jshumdrum123@gmail.com>
2022-04-27 12:21:51 +0100

    Run server command changed

--- app.py
+++ app.py
@@ -376,4 +376,4 @@ def update_graph(status, orbit, satname, satcatid,
 
 ## --- Run App ----
 
-app.run_server(debug=True, host="0.0.0.0", port=8050)
+if __name__ == "__main__": app.run_server(debug=False, host='0.0.0.0', port=8050)
